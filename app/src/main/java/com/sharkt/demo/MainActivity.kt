4e167fd62fe871dcea2096fd8bc5c260daf52def
renyuxiang <renyuxiang@yongche.com>
2019-11-27 16:10:21 +0800

    封装Api的Rx回调，封装Rx的自动IO异步+自动解绑

--- app/src/main/java/com/sharkt/demo/MainActivity.kt
+++ app/src/main/java/com/sharkt/demo/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.sharkt.demo
 
 import android.os.Bundle
-import com.sharkt.api.ApiResultObserver
-import com.sharkt.api.ApiService
-import com.sharkt.api.BaseDo
 import com.sharkt.api.GithubHttpManager
-import com.sharkt.lib_rx_kt.autoSubscribeObserveDisposeTransform
 import com.sharkt.mvvm.BaseMvvmActivity
 
 
@@ -17,26 +13,27 @@ class MainActivity : BaseMvvmActivity<MainVm>() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        headMp["Accept"] = "application/vnd.github.v3+json"
-
-        GithubHttpManager.init(this.applicationContext, headMp)
-        var api = GithubHttpManager.getInstance()!!.createAPI(ApiService::class.java)
-        api.getEmojis()
-            .autoSubscribeObserveDisposeTransform(this)
-            .subscribe(object : ApiResultObserver<Any>() {
-                override fun onSuccess(t: BaseDo<Any>) {
-                }
-
-                override fun onFail(e: Throwable) {
-                }
-
-                override fun onComplete() {
-                }
-
-            })
+//        headMp["Accept"] = "application/vnd.github.v3+json"
+//
+//        GithubHttpManager.init(this.applicationContext, headMp)
+//        var api = GithubHttpManager.getInstance()!!.createAPI(ApiService::class.java)
+//        api.getEmojis()
+//            .autoSubscribeObserveDisposeTransform(this)
+//            .subscribe(object : ApiResultObserver<Any>() {
+//                override fun onSuccess(t: BaseDo<Any>) {
+//                }
+//
+//                override fun onFail(e: Throwable) {
+//                }
+//
+//                override fun onComplete() {
+//                }
+//
+//            })
     }
 
     override fun init() {
+
     }
 
     override fun bind() {
--- app/src/test/java/com/sharkt/demo/ExampleUnitTest.kt
+++ app/src/test/java/com/sharkt/demo/ExampleUnitTest.kt
@@ -1,9 +1,8 @@
 package com.sharkt.demo
 
-import android.widget.TextView
+import kotlinx.coroutines.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,6 +13,69 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testCoroutine() {
+//        GlobalScope.launch {
+//            delay(1000)
+//            print("world")
+//        }
+//        print("Hello ")
+//        Thread.sleep(2000)
+//        print(" !")
+
+//        GlobalScope.launch { // launch a new coroutine in background and continue
+//            delay(1000L)
+//            println("World!")
+//        }
+//        println("Hello,") // main thread continues here immediately
+//        runBlocking {     // but this expression blocks the main thread
+//            delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+//        }
+    }
+
+    @Test
+    fun runCombine() = runBlocking { // this: CoroutineScope
+        runBlocking {
+            delay(200L)
+            println("Task from runBlocking")
+        }
+
+        coroutineScope { // Creates a coroutine scope
+            launch {
+                delay(500L)
+                println("Task from nested launch")
+            }
+
+            delay(100L)
+            println("Task from coroutine scope") // This line will be printed before the nested launch
+        }
+
+        println("Coroutine scope is over") // This line is not printed until the nested launch completes
+    }
+
+    @Test
+    fun runJoin() = runBlocking {
+        val job = GlobalScope.launch {
+            // launch a new coroutine and keep a reference to its Job
+            delay(1000)
+            println("World!")
+        }
+        job.join()
+        println("Hello,")
+    }
+
+    @Test
+    fun runBlock() = runBlocking<Unit> {
+        // start main coroutine
+        GlobalScope.launch {
+            // launch a new coroutine in background and continue
+            delay(1000L)
+            println("World!")
+        }
+        println("Hello,") // main coroutine continues here immediately
+        delay(2000L)      // delaying for 2 seconds to keep JVM alive
     }
 
     @Test
@@ -89,7 +151,5 @@ class ExampleUnitTest {
         }.apply {
 
         }
-
     }
-
 }
